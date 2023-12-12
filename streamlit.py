c4ddc7a3dc2b59b427f9b155f0577f5a84e7bc71
Habsoum64 <93042836+Habsoum64@users.noreply.github.com>
2023-12-12 10:27:15 +0000

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,19 +1,7 @@
 import streamlit as st
-import cv2
-import numpy as np
+import PIL
+from ultralytics import YOLO
 
-# Placeholder for object detection function
-def perform_object_detection(image):
-    # Replace this function with your actual object detection logic
-    # The function should take an image as input and return the detection results
-
-    # Example: Detect faces using Haarcascades classifier
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-    faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-
-    detection_results = [(f"Face {i + 1}", tuple(face)) for i, face in enumerate(faces)]
-    return detection_results
 
 def main():
     st.title("Object Detection App")
@@ -21,23 +9,14 @@ def main():
     uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
 
     if uploaded_file is not None:
-        # Read the image
-        image_np = cv2.imdecode(np.frombuffer(uploaded_file.read(), np.uint8), cv2.IMREAD_COLOR)
+        model = YOLO('yolo_model.pt')
+        image = PIL.Image.open(uploaded_file)
 
-        # Perform object detection
-        detection_results = perform_object_detection(image_np)
+        results = model.predict(image, show=True)
+        results_plot = results[0].plot()[:, :, ::-1]
 
-        # Convert BGR to RGB
-        image_rgb = cv2.cvtColor(image_np, cv2.COLOR_BGR2RGB)
+        st.image(results_plot, caption=(str(len(results[0].boxes)) + " objects detected"))
 
-        # Draw rectangles around detected objects
-        for _, (x, y, w, h) in detection_results:
-            cv2.rectangle(image_rgb, (x, y), (x + w, y + h), (255, 0, 0), 2)
-
-        # Display the image with rectangles
-        st.image(image_rgb, caption="Uploaded Image with Detected Objects", use_column_width=True)
 
 if __name__ == "__main__":
     main()
-
-    
