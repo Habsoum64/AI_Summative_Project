b6c46458bdea1a71e001bef761c34547bf7ff4ee
Habsoum64 <Habsoum64@users.noreply.github.com>
2023-12-12 11:24:28 +0000

    update

---
+++ obj_detect_app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import PIL
+from ultralytics import YOLO
+
+
+def main():
+    st.title("Object Detection App")
+
+    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
+
+    if uploaded_file is not None:
+        model = YOLO('yolo_model.pt')
+        image = PIL.Image.open(uploaded_file)
+
+        results = model.predict(image, show=True)
+        results_plot = results[0].plot()[:, :, ::-1]
+
+        st.image(results_plot, caption=(str(len(results[0].boxes)) + " objects detected"))
+
+
+if __name__ == "__main__":
+    main()
+
+    
